eda79bf2f32514e68458ec6f1012d24ae20853f0
KentRegs <kentregalado@protonmail.com>
2018-09-17 22:05:01 +0800

    edited index.js

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+var app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
