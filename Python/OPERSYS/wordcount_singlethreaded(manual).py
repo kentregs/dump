3f5ca43156082c256f87140d975b06c2479a5bf3
Kent Regalado <matthewregalado90@yahoo.com>
2020-04-27 16:12:59 +0800

    Update wordcount_singlethreaded(manual).py

--- Python/OPERSYS/wordcount_singlethreaded(manual).py
+++ Python/OPERSYS/wordcount_singlethreaded(manual).py
@@ -1,8 +1,6 @@
 # single-threaded manual approach
 # multi-threaded approach
-# authors:  Kent Regalado
-#           Eduardo Te
-#           Seth Gabon
+# author:  Kent Regalado
 # margins of error:
 #   word count:         295,046 - 283,180     = 11,866
 #   sentence count:     19,769 - 19,435       = 334
